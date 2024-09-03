c2b7defbda2934a3929dbf196d512df40d6b6949
aminulhoque786 <aminulsara6@gmail.com>
2024-09-03 16:15:51 +0600

    changes some code

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,32 @@
-
-import './App.css'
-import Banner from './components/banner/Banner'
-import Client from './components/client/Client'
-import Experience from './components/experience/Experience'
-
-import Header from './components/header/Header'
-import Price from './components/price/Price'
-import Recent from './components/recent/Recent'
-import Skills from './components/skills/Skills'
-import Talk from './components/talk/Talk'
-
+import "./App.css";
+import Banner from "./components/banner/Banner";
+import Client from "./components/client/Client";
+import Experience from "./components/experience/Experience";
+import { Sugar } from "react-preloaders";
+import Header from "./components/header/Header";
+import Price from "./components/price/Price";
+import Recent from "./components/recent/Recent";
+import Skills from "./components/skills/Skills";
+import Talk from "./components/talk/Talk";
+import React, { useState, useEffect } from "react";
 function App() {
-
 
   return (
     <>
-   <Header/>
-   <Banner/>
-  <Experience/>
-
-  <Recent/>
-  <Price/>
-  <Skills/>
-  <Client/>
-  <Talk/>
+    
+        <div >
+          <Header />
+          <Banner />
+          <Experience />
+          <Recent />
+          <Price />
+          <Skills />
+          <Client />
+          <Talk />
+        </div>
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
